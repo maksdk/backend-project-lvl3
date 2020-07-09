7dae1591eb99b9665fd535fe4ce61fa01ee55cb3
maksdk <havryshchenko@gmail.com>
2020-07-09 21:56:49 +0300

    Created page loader

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 // @ts-check
+import { promises as fs } from 'fs';
+import path from 'path';
+import axios from 'axios';
+
+const generateFileName = (href) => href.replace(/(^\w+:|^)\/\//, '').replace(/[^A-Za-z\d]/g, '-');
 
 export default (href, outputPath) => {
-  console.log('href', href);
-  console.log('outputPath', outputPath);
+  axios.get(href)
+    .then((res) => res.data)
+    .then((data) => fs.writeFile(path.join(outputPath, `${generateFileName(href)}.html`), data))
+    .catch((err) => {
+      console.log(err);
+    });
 };
